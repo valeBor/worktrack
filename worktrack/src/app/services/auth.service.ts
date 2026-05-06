3e69077b797dc01b4f1efa52bf76ca5a33528f09
Valeria Borgatti <valeriamborgatti3@gmail.com>
2026-05-05 22:23:35 -0300

    Actualización database

--- worktrack/src/app/services/auth.service.ts
+++ worktrack/src/app/services/auth.service.ts
@@ -14,27 +14,27 @@ export class AuthService {
 
   constructor(private http: HttpClient) {}
 
-  // 🔐 LOGIN
+  //  LOGIN
   login(user: User): Observable<AuthResponse> {
     return this.http.post<AuthResponse>(`${this.api}/login`, user);
   }
 
-  // 💾 GUARDAR SESIÓN
+  //  GUARDAR SESIÓN
   saveUser(data: AuthResponse) {
     localStorage.setItem('user', JSON.stringify(data));
   }
 
-  // 📥 OBTENER USUARIO
+  //  OBTENER USUARIO
   getUser(): AuthResponse | null {
     return JSON.parse(localStorage.getItem('user') || 'null');
   }
 
-  // 🚪 LOGOUT
+  // LOGOUT
   logout() {
     localStorage.removeItem('user');
   }
 
-  // ✅ SABER SI ESTÁ LOGUEADO
+  // SABER SI ESTÁ LOGUEADO
   isLoggedIn(): boolean {
     return !!localStorage.getItem('user');
   }
