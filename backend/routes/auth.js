b826d3ee6f061cc46735aae8eaf65b018087c369
Valeria Borgatti <valeriamborgatti3@gmail.com>
2026-05-13 12:39:00 -0300

    Dashboard Admin

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db');
 
-// 🔐 LOGIN
+//  LOGIN
 router.post('/login', (req, res) => {
   const { email, password } = req.body;
 
--- worktrack/src/app/pages/admin/admin.ts
+++ worktrack/src/app/pages/admin/admin.ts
@@ -1,10 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-admin',
   standalone:true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './admin.html',
   styleUrl: './admin.css',
 })
-export class Admin {}
+export class Admin {
+
+
+
+
+  
+}
