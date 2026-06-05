5925eebbcec9f787be52165fd0ea48a494c3b69a
Brisa Zubiri <142553474+brisa10112002@users.noreply.github.com>
2026-06-05 20:43:28 -0300

    agrego fronted estático del scanner

--- worktrack/src/app/pages/scanner/scanner.ts
+++ worktrack/src/app/pages/scanner/scanner.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-scanner',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   imports: [],
   templateUrl: './scanner.html',
   styleUrl: './scanner.css',
+  encapsulation: ViewEncapsulation.None,
 })
 export class Scanner {}
